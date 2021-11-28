bbe5bf17d71c9969ae588eea4c75a06eb4a0fab2
kjeganathan <68821572+kjeganathan@users.noreply.github.com>
2021-11-28 01:04:28 -0500

    changes for heroku

--- express.js
+++ express.js
@@ -18,14 +18,20 @@ app.post("/api/createAccount", (req, res) => {
     console.log(data.firstname);
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "./client/public/index.html"));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, "client/build", "./client/public/index.html"));
+// });
 
 //serves all the 
 // app.use((req, res, next) => {
 //     res.sendFile(path.join(__dirname, "..", "./evalu8/client/build", "index.html"));
 // });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+  });
+
+// app.use(express.static(path.join(__dirname, '../client/build', "index.html")));
 
 const port = 3000; // specify the port 
 app.listen(process.env.PORT || port);
