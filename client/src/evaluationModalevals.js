c28936969e145b7496c77fe07985d503cac3e154
kjeganathan <68821572+kjeganathan@users.noreply.github.com>
2022-03-18 04:08:44 -0400

    added more to evaluations code

--- client/src/evaluationModalevals.js
+++ client/src/evaluationModalevals.js
@@ -11,11 +11,36 @@ import React, { Component, useState, useEffect, useRef } from "react";
 const EvaluationModalEvals = (props) => {
 
   const [labelState, setLabel] = useState('Not Completed');
+  const [data, setData] = useState('false');
+  let flag = false;
+  let count = 1;
+   //sends in a single Object
+  let nameOfEval = props.evalType //can be manager, peer or goal setting eval, or eval title/name
+  let numOfEvals = props.evalNum; //number of each evaluation
+  let outArrType = nameOfEval[0];
 
      //set checked to false in the beginning
     //const checked = React.useState(false);
     const handleEvalClick = (event) => {
       let checked = event.target.checked;
+      // let checked = data;
+      // let tempflag = false;
+      // let tempchecked = false;
+      // if(flag == true){
+      //   tempflag = false;
+      // }else{
+      //   tempflag = true;
+      // }
+
+      // if(checked == true){
+      //   tempchecked = false;
+      // }else{
+      //   tempchecked = true;
+      // }
+
+      // flag = tempflag;
+      // checked = tempchecked;
+
       console.log(checked);
       if(checked == true){
         setLabel('Completed');
@@ -32,12 +57,57 @@ const EvaluationModalEvals = (props) => {
 
   }
 
+  let data2 = {teammemberinfo: props.sendTeamMember, evaltype: outArrType, evalnumber: props.evalNum}
+  useEffect(() => {
+    console.log('use effect ran');
+    
+  let getChecked = async () => {
+    await fetch("/api/getChecked", {
+      method: "POST",
+      body: JSON.stringify(data2),
+      headers: { "Content-Type": "application/json" },
+    }).then(async (response) => 
+    {
+      let resp = await response.json();
+       // the response is the ischecked value
+      if(resp.length != 0){ //not undefined
+        localStorage.setItem('bool', JSON.stringify(resp[0]["ischecked"]));
+       
+        console.log("truebool:" + JSON.parse(localStorage.getItem('bool')));
+      }else{
+        localStorage.setItem('bool', JSON.stringify(false));
+      
+        console.log("falsebool:" + JSON.parse(localStorage.getItem('bool')));
+      }
+
+      console.log('finalobj:'+ JSON.parse(localStorage.getItem('bool')));
+      let newData = JSON.parse(localStorage.getItem('bool'));
+      if(newData != data){
+        setData(newData);
+      }
+      
+      console.log(data);
+      
+    });
+  }
+    getChecked();
+    let ischecked = JSON.parse(localStorage.getItem('bool'));
+    console.log("ischecked:" + data);
+  }, [data]);
+
+  if(data == false){
+    flag = false;
+  }else if(data == true){
+    flag = true;
+  }else{
+    flag = false;
+  }
+  console.log("flag2:"+flag);
+ 
+
   //pass in prop
    //object of type of eval and the number of each eval
-  let count = 1;
-   //sends in a single Object
-  let nameOfEval = props.evalType //can be manager, peer or goal setting eval, or eval title/name
-  let numOfEvals = props.evalNum; //number of each evaluation
+  
 
   //DO CALCULATION BASED ON NUMBER OF EVALS HERE TO GET THE PERCENTAGE
   
@@ -53,6 +123,7 @@ const EvaluationModalEvals = (props) => {
       type="switch"
       onChange={handleEvalClick}
       label={labelState}
+      // checked={flag}
       id={props.sendTeamMember + "," + nameOfEval + "," + numOfEvals}
     />
 
