486fae5bb5796a6d5e19fe725338b45a16b81fe7
kjeganathan <68821572+kjeganathan@users.noreply.github.com>
2022-04-30 23:41:46 -0400

    added modal design changes for homepage

--- client/src/homePage.js
+++ client/src/homePage.js
@@ -42,9 +42,6 @@ class homePage extends Component {
     this.props.history.push("/createAccount");
   };
 
-
-
-
   render() {
     return (
       <div>
@@ -88,22 +85,53 @@ class homePage extends Component {
                 onHide={this.handleClose}
                 backdrop="static"
                 keyboard={false}
-                size="lg"
+                size="md"
                 aria-labelledby="contained-modal-title-vcenter"
                 centered
+                className="homepageModal"
               >
                 <Modal.Header closeButton>
-                  <Modal.Title>Are you a:</Modal.Title>
+                  <Modal.Title>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="26"
+                      height="26"
+                      fill="currentColor"
+                      class="bi bi-person-plus-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                      <path
+                        fill-rule="evenodd"
+                        d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
+                      />
+                    </svg>
+                    &nbsp;
+                    Sign Up
+                  </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <center>
-                <Button variant="secondary" onClick={this.routeChangeToAdminCreateAccount}>
-                    Professor
-                  </Button>
-                  <Link to="/adminCreateAccount"/>
-                  &nbsp; OR &nbsp;
-                  <Button variant="primary" onClick={this.routeChangeToStudentCreateAccount}>Student</Button>
-                  
+                  <center>
+                    <br />
+                    <Button
+                      className="btn btn-dark"
+                      id="professor-signup"
+                      onClick={this.routeChangeToAdminCreateAccount}
+                    >
+                      I am a Professor
+                    </Button>
+                    &nbsp; &nbsp; &nbsp;
+                    <Link to="/adminCreateAccount" />
+                    &nbsp;
+                    <Button
+                      className="btn btn-dark"
+                      id="student-signup"
+                      onClick={this.routeChangeToStudentCreateAccount}
+                    >
+                      I am a Student
+                    </Button>
+                    <br />
+                    <br />
                   </center>
                 </Modal.Body>
               </Modal>
